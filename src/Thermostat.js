cbbec2f32d6a3d17260493dcb7c1cefc67a15603
ainsleybc <ab.chang@me.com>
2017-10-25 22:11:49 +0100

    refactored prototype

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -6,38 +6,42 @@ function Thermostat() {
   this._powerSavingModeLimit = 25;
 }
 
-Thermostat.prototype.powerSavingMode = function(mode) {
-   if (mode == 'on') {
-     this._powerSavingModeLimit = 25;
-   } else if (mode == 'off') {
-     this._powerSavingModeLimit = 32;
-   }
-}
+Thermostat.prototype = {
+  
+  powerSavingMode: function (mode) {
+    if (mode == 'on') {
+      this._powerSavingModeLimit = 25;
+    } else if (mode == 'off') {
+      this._powerSavingModeLimit = 32;
+    }
+  },
 
-Thermostat.prototype.temperature = function() {
-  return this._temperature;
-}
+  temperature: function () {
+    return this._temperature;
+  },
 
-Thermostat.prototype.up = function() {
-  if (this._temperature >= this._powerSavingModeLimit) throw new Error('Maximum temperature is '+this._powerSavingModeLimit+' degrees');
-  this._temperature++;
-}
+  up: function() {
+    if (this._temperature >= this._powerSavingModeLimit) throw new Error('Maximum temperature is '+this._powerSavingModeLimit+' degrees');
+    this._temperature++;
+  },
 
-Thermostat.prototype.down = function () {
-  if (this._temperature === 10) throw new Error('Minimum temperature is 10');
-  this._temperature--;
-}
+  down: function () {
+    if (this._temperature === 10) throw new Error('Minimum temperature is 10');
+    this._temperature--;
+  },
 
-Thermostat.prototype.reset = function () {
-  this._temperature = this._DEFAULT_TEMPERATURE;
-}
+  reset: function () {
+    this._temperature = this._DEFAULT_TEMPERATURE;
+  },
 
-Thermostat.prototype.energyUsage = function () {
-  if (this._temperature < 18) {
-    return 'low-usage';
-  } else if (this._temperature < 25) {
-    return 'medium-usage';
-  } else {
-    return 'high-usage';
+  energyUsage: function () {
+    if (this._temperature < 18) {
+      return 'low-usage';
+    } else if (this._temperature < 25) {
+      return 'medium-usage';
+    } else {
+      return 'high-usage';
+    }
   }
-}
+
+};
